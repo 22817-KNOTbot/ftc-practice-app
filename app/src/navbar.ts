791984cdf3cab3bbf8b9ce2476f82965a332330d
hotwh-et <ethan10.wong@gmail.com>
2025-10-19 19:17:33 -0700

    Fix navbar sizing

--- app/src/navbar.ts
+++ app/src/navbar.ts
@@ -1,5 +1,10 @@
 export function registerNavbar(navbar: HTMLElement) {
 	navbar.addEventListener("mouseenter", () => {
+		let heightSum = 0;
+		for (const child of navbar.children) {
+			heightSum += (child as HTMLElement).offsetHeight;
+		}
+		navbar.style.setProperty("--total-height", `${heightSum}px`);
 		navbar.classList.remove("collapsed");
 	});
 

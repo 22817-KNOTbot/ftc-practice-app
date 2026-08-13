87b023416ee9d5df7743fc6578524fe59aa84c53
hotwh-et <ethan10.wong@gmail.com>
2026-08-12 20:35:54 -0700

    Add dist folder to gitignore as no longer needed. Optimize build time

--- app/dist/assets/about.js
+++
@@ -1,1 +0,0 @@
-import{g as o,a as l,r as s}from"./navbar.js";const r=o("layout"),y=l(r),t=y.layoutDataGetter();let e="";typeof t.stylePath=="object"?t.stylePath.forEach(a=>{e+=`<link rel="stylesheet" href="${a}">`}):e=`<link rel="stylesheet" href="${t.stylePath}">`;document.querySelector("#app").innerHTML=e+t.html.about;s(document.querySelector("nav"));
